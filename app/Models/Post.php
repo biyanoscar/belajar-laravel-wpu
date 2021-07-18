1082e806cf4047f21bdd5e293ea045bdfea91145
LAPTOP-MRCNFPFF\biyan <biyanoscar@gmail.com>
2021-07-18 12:30:44 +0700

    5a. Model - static data & method

---
+++ app/Models/Post.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+
+class Post
+{
+    // use HasFactory;
+    private static $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Sandhika Galih",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum beatae recusandae reiciendis fugiat quas provident ipsa ad sed neque fuga nobis culpa consectetur commodi corrupti, perspiciatis exercitationem velit autem, sunt dolorem ipsum eius labore. Eveniet earum assumenda, ex beatae expedita quis tenetur eius ut, sapiente quisquam voluptate debitis perspiciatis odit. Facere, exercitationem delectus! Corporis nulla quo soluta dolores nisi, inventore, rerum adipisci aperiam quae voluptates corrupti, voluptas aut quisquam cumque non tempora ad officia veritatis illo commodi nostrum sint sed?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Doddy Ferdiansyah",
+            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim similique porro esse reiciendis aut eveniet excepturi soluta. Consequatur ipsa quis sunt magnam architecto obcaecati quae, fuga, inventore voluptatum velit odio. Itaque id assumenda debitis corporis eum impedit praesentium explicabo et unde dolorem nemo quasi suscipit totam veritatis asperiores, alias error dolor enim sequi, officia pariatur voluptatum provident? Itaque commodi eveniet error sed earum aut nam quo dolorem architecto cumque! Quae earum harum ea odit, cupiditate error nostrum dolores minima. Fugiat sit a reprehenderit voluptatibus repellendus sequi et velit, praesentium explicabo debitis corporis! Dolore recusandae earum unde quaerat in tempore eius?"
+        ],
+    ];
+
+    public static function all()
+    {
+        return self::$blog_posts;
+    }
+
+    public static function find($slug)
+    {
+        $posts = self::$blog_posts;
+        $post = [];
+        foreach ($posts as $p) {
+            if ($p["slug"] === $slug) {
+                $post = $p;
+            }
+        }
+        
+        return $post;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,55 +35,19 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Sandhika Galih",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum beatae recusandae reiciendis fugiat quas provident ipsa ad sed neque fuga nobis culpa consectetur commodi corrupti, perspiciatis exercitationem velit autem, sunt dolorem ipsum eius labore. Eveniet earum assumenda, ex beatae expedita quis tenetur eius ut, sapiente quisquam voluptate debitis perspiciatis odit. Facere, exercitationem delectus! Corporis nulla quo soluta dolores nisi, inventore, rerum adipisci aperiam quae voluptates corrupti, voluptas aut quisquam cumque non tempora ad officia veritatis illo commodi nostrum sint sed?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Doddy Ferdiansyah",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim similique porro esse reiciendis aut eveniet excepturi soluta. Consequatur ipsa quis sunt magnam architecto obcaecati quae, fuga, inventore voluptatum velit odio. Itaque id assumenda debitis corporis eum impedit praesentium explicabo et unde dolorem nemo quasi suscipit totam veritatis asperiores, alias error dolor enim sequi, officia pariatur voluptatum provident? Itaque commodi eveniet error sed earum aut nam quo dolorem architecto cumque! Quae earum harum ea odit, cupiditate error nostrum dolores minima. Fugiat sit a reprehenderit voluptatibus repellendus sequi et velit, praesentium explicabo debitis corporis! Dolore recusandae earum unde quaerat in tempore eius?"
-        ],
-    ];
-
+Route::get('/posts', function () {
     return view('posts', [
         "title" => "Posts",
-        "posts" => $blog_posts
+        "posts" => Post::all(),
     ]);
 });
 
 //halaman single post
 Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Sandhika Galih",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum beatae recusandae reiciendis fugiat quas provident ipsa ad sed neque fuga nobis culpa consectetur commodi corrupti, perspiciatis exercitationem velit autem, sunt dolorem ipsum eius labore. Eveniet earum assumenda, ex beatae expedita quis tenetur eius ut, sapiente quisquam voluptate debitis perspiciatis odit. Facere, exercitationem delectus! Corporis nulla quo soluta dolores nisi, inventore, rerum adipisci aperiam quae voluptates corrupti, voluptas aut quisquam cumque non tempora ad officia veritatis illo commodi nostrum sint sed?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Doddy Ferdiansyah",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim similique porro esse reiciendis aut eveniet excepturi soluta. Consequatur ipsa quis sunt magnam architecto obcaecati quae, fuga, inventore voluptatum velit odio. Itaque id assumenda debitis corporis eum impedit praesentium explicabo et unde dolorem nemo quasi suscipit totam veritatis asperiores, alias error dolor enim sequi, officia pariatur voluptatum provident? Itaque commodi eveniet error sed earum aut nam quo dolorem architecto cumque! Quae earum harum ea odit, cupiditate error nostrum dolores minima. Fugiat sit a reprehenderit voluptatibus repellendus sequi et velit, praesentium explicabo debitis corporis! Dolore recusandae earum unde quaerat in tempore eius?"
-        ],
-    ];
-
-    $new_post=[];
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] == $slug) {
-            $new_post=$post;
-        }
-    }
 
     return view('post', [
         "title" => "Single Post",
-        "post" => $new_post
+        "post" => Post::find($slug),
     ]);
 });
 
